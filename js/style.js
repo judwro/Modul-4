fc3e78bfa766fc3ef96ce6b63e2c03093abb04db
Judyta Wrobel <judwro@interia.pl>
2022-11-28 19:41:29 +0100

    reduce blank lines between lines.

--- js/style.js
+++ js/style.js
@@ -1,7 +1,6 @@
 {
     console.log("Hello!");
 
-
     const calculateResult = (amount, currency) => {
         const rateEUR = 4.72;
         const rateUSD = 4.74;
@@ -28,33 +27,24 @@
             resultElement.innerHTML = `${amount} PLN = <strong>${result.toFixed(2)} ${currency} </strong>`;
         }
 
-
-
         const onFormSubmit = (event) => {
             event.preventDefault();
 
             const amountElement = document.querySelector(".js-amount")
             const currencyElement = document.querySelector(".js-currency")
-
-
             const amount = amountElement.value;
             const currency = currencyElement.value;
             const result = calculateResult(amount, currency);
 
             updateResultText(amount, result, currency);
-
-
         }
 
         const init = () => {
             const formElement = document.querySelector(".js-form")
 
             formElement.addEventListener("submit", onFormSubmit);
-
         };
 
         init();
     }
-
-
 }
